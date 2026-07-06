e98467f902f1ec79d020b93a99bb7c22ff295be3
AlivaIndu <alivaindu@gmail.com>
2026-07-06 15:23:26 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.java
@@ -0,0 +1,29 @@
+class MinStack {
+    Stack<Integer> s1 ;
+    Stack<Integer> s2 ;
+    public MinStack() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s1.push(val);
+        if(s2.isEmpty() || val<=s2.peek()){
+            s2.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(s1.isEmpty()) return;
+        if(s2.peek().equals(s1.pop()))
+            s2.pop();
+    }
+    
+    public int top() {
+        return s1.peek();
+    }
+    
+    public int getMin() {
+        return s2.peek();
+    }
+}
